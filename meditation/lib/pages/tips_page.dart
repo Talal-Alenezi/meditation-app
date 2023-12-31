3f53b585184e6a4399f75e8a220099ef4c2d8aed
Talal Alenezi <cpe.talal.alenezi@gmail.com>
2023-12-31 15:49:18 +0300

    show only my tips is done

--- meditation/lib/pages/tips_page.dart
+++ meditation/lib/pages/tips_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+import 'package:meditation/providers/auth_povider.dart';
 import 'package:meditation/providers/tips_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:meditation/models/tips_model.dart';
 
 class TipsPage extends StatefulWidget {
   const TipsPage({Key? key}) : super(key: key);
@@ -9,8 +12,16 @@ class TipsPage extends StatefulWidget {
   State<TipsPage> createState() => _TipsPageState();
 }
 
-class _TipsPageState extends State<TipsPage> {
+class _TipsPageState extends State<TipsPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   TextEditingController tipController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
 
   void _showAddTipDialog(BuildContext context) {
     showDialog(
@@ -32,7 +43,6 @@ class _TipsPageState extends State<TipsPage> {
             ElevatedButton(
               onPressed: () {
                 String newTip = tipController.text;
-                print(newTip);
                 Navigator.of(context).pop(); // Close the dialog
                 if (newTip.isNotEmpty) {
                   context.read<TipsProvider>().addTip(newTip);
@@ -57,56 +67,132 @@ class _TipsPageState extends State<TipsPage> {
             onPressed: () => _showAddTipDialog(context),
           ),
         ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'All Tips'),
+            Tab(text: 'My Tips'),
+          ],
+        ),
       ),
-      body: FutureBuilder(
-        future: context.read<TipsProvider>().getTips(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.error != null) {
-            return const Center(child: Text('An error occurred'));
-          } else {
-            return Consumer<TipsProvider>(
-              builder: (context, tipsProvider, child) {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  // physics: const NeverScrollableScrollPhysics(),
-                  itemCount: tipsProvider.tipsList.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: ListTile(
-                        title: Text("${tipsProvider.tipsList[index].text}"),
-                        subtitle: Text(
-                            'Author: ${tipsProvider.tipsList[index].author}'),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.arrow_drop_up),
-                            ),
-                            Text(
-                              '${tipsProvider.tipsList[index].upvotes?.length}',
-                            ),
-                            SizedBox(width: 10),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.arrow_drop_down),
-                            ),
-                            Text(
-                              '${tipsProvider.tipsList[index].downvotes?.length}', // Replace with actual downvote count
-                            ),
-                          ],
-                        ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildTipsList(context),
+          _buildMyTipsList(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTipsList(BuildContext context) {
+    return FutureBuilder(
+      future: context.read<TipsProvider>().getTips(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.error != null) {
+          return const Center(child: Text('An error occurred'));
+        } else {
+          return Consumer<TipsProvider>(
+            builder: (context, tipsProvider, child) {
+              return ListView.builder(
+                shrinkWrap: true,
+                // physics: const NeverScrollableScrollPhysics(),
+                itemCount: tipsProvider.tipsList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text("${tipsProvider.tipsList[index].text}"),
+                      subtitle: Text(
+                          'By: ${tipsProvider.tipsList[index].author}'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_drop_up),
+                          ),
+                          Text(
+                            '${tipsProvider.tipsList[index].upvotes?.length}',
+                          ),
+                          SizedBox(width: 10),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_drop_down),
+                          ),
+                          Text(
+                            '${tipsProvider.tipsList[index].downvotes?.length}', // Replace with actual downvote count
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                );
-              },
-            );
-          }
-        },
-      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildMyTipsList(BuildContext context) {
+    String username = '';
+    final token = context.watch<AuthProvider>().token;
+    if (token.isNotEmpty) {
+      Map<String, dynamic>? decodedToken = Jwt.parseJwt(token);
+      username = decodedToken['username'];
+      print('Username from token: $username');
+    }
+    return FutureBuilder(
+      future: context.read<TipsProvider>().myTips(username),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.error != null) {
+          return const Center(child: Text('An error occurred'));
+        } else {
+          return Consumer<TipsProvider>(
+            builder: (context, tipsProvider, child) {
+              return ListView.builder(
+                shrinkWrap: true,
+                // physics: const NeverScrollableScrollPhysics(),
+                itemCount: tipsProvider.myTipsList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text("${tipsProvider.myTipsList[index].text}"),
+                      subtitle: Text(
+                          'By: ${tipsProvider.myTipsList[index].author}'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_drop_up),
+                          ),
+                          Text(
+                            '${tipsProvider.myTipsList[index].upvotes?.length}',
+                          ),
+                          SizedBox(width: 10),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_drop_down),
+                          ),
+                          Text(
+                            '${tipsProvider.myTipsList[index].downvotes?.length}', // Replace with actual downvote count
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        }
+      },
     );
   }
 }
--- meditation/lib/providers/tips_provider.dart
+++ meditation/lib/providers/tips_provider.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+
 import 'package:meditation/models/tips_model.dart';
 import 'package:meditation/services/tips_service.dart';
 
 class TipsProvider extends ChangeNotifier {
   List<Tips> tipsList = [];
+  List<Tips> myTipsList = [];
 
   final _tipsService = TipsServices();
+
   Future<void> getTips() async {
     tipsList = await _tipsService.getTips();
-
     notifyListeners();
   }
 
-  Future<void> addTip(String newTip) async {
-    try {
-      await _tipsService.addTip(newTip);
-      await getTips(); // Update the tips list after adding a new tip
-    } catch (e) {
-      print('Error adding tip: $e');
-      // Handle the error as needed, for example, show a snackbar
-    }
+  Future<void> myTips(String username) async {
+    final List<Tips> allTips = await _tipsService.getTips();
+
+    myTipsList = allTips.where((tip) => tip.author == username).toList();
+    notifyListeners();
+  }
+
+  addTip(newTip) async {
+    await _tipsService.addTip(newTip);
+    getTips();
   }
 }
